c13f6d8ea24e2bf887bed2fbd150e4f6566ba275
andrewmpierce <apierce1992@gmail.com>
2016-02-22 20:19:01 -0500

    able to move image with keydown

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import sys, pygame;
 from pygame.locals import *;
+#import player;
+
 pygame.init()
 
 size = width, height = 500, 400
 speed = [2, 2]
 black = 0, 0, 0
+white = 100, 100, 100
 screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("images/ball.bmp")
 ballrect = ball.get_rect()
+pygame.key.set_repeat(10,10)
 
 while 1:
   for event in pygame.event.get():
-    if event.type == pygame.QUIT: sys.exit()
-    if event.key == K_RIGHT:
-      ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-      speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-      speed[1] = -speed[1]
+      if event.type == pygame.QUIT: sys.exit()
+      if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_k:
+              ballrect.x = ballrect.x +speed[0]
 
   screen.fill(black)
   screen.blit(ball, ballrect)
